0172f97238fa581e46fada6712bc14cfb3b4bd6b
elvidence <39727826+elvidence@users.noreply.github.com>
2024-09-12 12:40:12 +1000

    Create misp_worker_monitor.py
    MISP Worker Monitor
    
    Overview
    
    misp_worker_monitor.py is a Python script designed to ensure the reliability of MISP workers by monitoring their status and attempting to restart them if they are found to be down. This tool plays a critical role in maintaining the operational health of MISP instances, especially in large deployments.
    
    Features
    
    Continuous Monitoring: Regularly checks the status of each MISP worker and logs their state.
    
    Automatic Recovery: Attempts to restart workers automatically if they are down.
    
    Notification System: Sends email alerts if a worker fails to restart after multiple attempts.
    
    Logging: Records all worker states and actions to a log file for audit and troubleshooting purposes.
    
    **Installation
    
    The script should be placed in a directory accessible to the MISP user (typically www-data on Ubuntu systems) and be made executable. It can be integrated into the system's crontab to run at desired intervals, ensuring continuous monitoring without manual intervention.
    
    **Configuration
    
    Modify the following variables within the script to suit your environment:
    
    workers: List of worker names as defined in your MISP setup.
    email_enabled: Enable or disable email notifications.
    
    log_file: Path to the log file.
    
    receiver_email: Email addresses of the notification recipients.
    
    **Usage
    
    To deploy, simply add an entry to your crontab:
    
    */5 * * * * /usr/bin/python3 /path/to/misp_worker_monitor.py
    
    This example runs the script every 5 minutes. Adjust the frequency as needed based on your operational requirements.
    
    **License
    
    This script is released under the MIT license. See LICENSE file for more details.

---
+++ misp_worker_monitor.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+"""
+Author: A.R.
+Version: 1.1
+Date: 14 Apr 2024
+License: MIT
+
+Description:
+This script monitors the status of MISP workers and attempts to restart them if they are down.
+It logs the worker status and sends email notifications if a worker fails to start after repeated attempts.
+The script is intended to be added to the crontab for the same user as MISP (typically www-data on Ubuntu installations).
+"""
+
+import subprocess
+import time
+import logging
+import smtplib
+import json
+from datetime import datetime
+from email.message import EmailMessage
+
+# Configuration
+workers = ['cache', 'default', 'email', 'prio', 'update', 'scheduler']
+email_enabled = True  # Set based on your need False/True
+log_file = '/var/log/misp_workers.log' # Set the log destination
+receiver_email = 'admin@yourorg.com.au', 'admin1@yourorg.com.au'  # Define the email address for notifications
+
+# Setup logging
+logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
+
+
+def check_worker_status(worker):
+    """Check the status of a worker."""
+    command = ['/var/www/MISP/app/Console/cake', 'Admin', 'getWorkers']
+    result = subprocess.run(command, capture_output=True, text=True)
+    try:
+        workers_status = json.loads(result.stdout)
+        worker_status = workers_status.get(worker, {}).get('ok', False)
+        return worker_status
+    except json.JSONDecodeError:
+        logging.error("Failed to decode JSON from worker status command.")
+        return False
+
+
+def start_worker(worker):
+    """Start a specific worker."""
+    command = ['/var/www/MISP/app/Console/cake', 'CakeResque.CakeResque', 'start', '--interval', '5', '--queue', worker]
+    subprocess.run(command)
+    logging.info(f"{worker} worker start command issued.")
+
+
+def send_email(subject, body):
+    """Send an email notification."""
+    if not email_enabled:
+        return
+    msg = EmailMessage()
+    msg.set_content(body)
+    msg['Subject'] = subject
+    msg['From'] = 'misp@yourorg.gov.au'
+    msg['To'] = receiver_email
+
+    # Set up the SMTP server (modify as needed for your configuration)
+    with smtplib.SMTP('localhost') as s:
+        s.send_message(msg)
+
+
+def manage_workers():
+    for worker in workers:
+        if not check_worker_status(worker):
+            logging.info(f"[{datetime.now()}] {worker} worker is down, attempting to start.")
+            start_worker(worker)
+            time.sleep(180)  # Wait for 3 minutes before checking again
+            if not check_worker_status(worker):
+                logging.info(f"[{datetime.now()}] {worker} worker failed to start on first attempt, retrying.")
+                time.sleep(1800)  # Wait for 30 minutes before retrying
+                start_worker(worker)
+                if not check_worker_status(worker):
+                    logging.error(f"[{datetime.now()}] {worker} worker failed to start after second attempt.")
+                    if email_enabled:
+                        send_email(f"{worker} Worker Failed", f"The {worker} worker failed to start after "
+                                                              f"repeated attempts.")
+                else:
+                    logging.info(f"[{datetime.now()}] {worker} worker started on second attempt.")
+            else:
+                logging.info(f"[{datetime.now()}] {worker} worker started successfully on first attempt.")
+
+
+if __name__ == "__main__":
+    manage_workers()
